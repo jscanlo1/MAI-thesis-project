2bad22741e07c1eb579672d1ae22c8072c35c9df
Johnny <johnnyscanlon22@gmail.com>
2022-04-13 12:48:08 +0100

    Bootstrapping

---
+++ bootstrap_testing.py
@@ -0,0 +1,20 @@
+import os
+import random
+import torch
+import dataset
+
+import numpy as np
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+
+import itertools
+
+import torch.nn as nn
+import torch.optim as optim
+
+#Import some libraries for calculating metrics
+from sklearn.metrics import f1_score,precision_score,accuracy_score
+from transformers import AdamW
+from transformers.optimization import get_linear_schedule_with_warmup
+from models.FakeNewsModel import FakeNewsModel
